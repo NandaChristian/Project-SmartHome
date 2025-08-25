3c38d62b618af98841ed35938fc39c598e33dfe2
NandaChristian <nandachristian4618@gmail.com>
2025-08-26 01:30:31 +0700

    first commit

--- utils/mqttClient.js
+++ utils/mqttClient.js
@@ -54,6 +54,17 @@ client.on('message', (topic, message) => {
     id_user: 3,
     waktu: new Date().toISOString(),
   };
+
+  const query = 'INSERT INTO logs (lokasi, status, waktu, konsumsi_daya, id_user) VALUES (?, ?, ?, ?, ?)';
+db.execute(query, [
+  data.lokasi,
+  data.status,
+  data.waktu.replace('T',' ').slice(0,19), // ubah ke format SQL 'YYYY-MM-DD HH:MM:SS'
+  data.konsumsi_daya,
+  data.id_user
+])
+.then(() => console.log('Log Google Assistant berhasil disimpan'))
+.catch((err) => console.error('Gagal simpan log dari MQTT:', err));
   
   broadcastToWebSocket(data);
 });
